a8d98281aa8624b3c33e4e85edfea9519a291a6e
Oscar Alexander Robles Braun <roblesbraun@gmail.com>
2023-04-10 14:23:30 -0600

    Added readCoordenates.h

---
+++ readCoordenates.h
@@ -0,0 +1,28 @@
+// Libreria para leer las coordenadas del acelerometro del telefono
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+float * readCoordenates () {
+    // Variables para las coordenadas x, y, z
+    float coordenates[2];
+    float *ptrCoordenates = coordenates;
+
+    // Leemos el archivo csv
+    FILE *fp;
+    float rx, ry;
+
+    fp = fopen("test.csv", "r");
+    if (fp == NULL)
+    {
+        printf("El archivo no se puede abrir \n");
+        exit(1);
+    }
+
+    while (fscanf(fp, "%f, %f", &rx, &ry) != EOF)
+    {
+        coordenates[0] = rx;
+        coordenates[1] = ry;
+    }
+    return (ptrCoordenates);
+}
--- readCsv.c
+++ readCsv.c
@@ -1,39 +1,14 @@
 
 // C program for the above approach
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "readCoordenates.h"
 
 // Driver Code
 int main()
 {
 
-    // Variables para las coordenadas x, y, z
-    float x = 0;
-    float y = 0;
-
-    // Leemos el archivo csv
-    FILE *fp;
-    float rx, ry;
-
-    while (true)
-    {
-        fp = fopen("coordenadas.csv", "r");
-        if (fp == NULL)
-        {
-            printf("El archivo no se puede abrir \n");
-            exit(1);
-        }
-
-        while (fscanf(fp, "%f, %f", &rx, &ry) != EOF)
-        {
-            x = rx;
-            y = ry;
-        }
-        printf("\nX: %f\n", x);
-        printf("\nY: %f\n", y);
-        fclose(fp);
-    }
-
+    float * coordenates = readCoordenates();
+    printf("\nX: %f\n", coordenates[0]);
+    printf("\nY: %f", coordenates[1]);
     return 0;
 }
