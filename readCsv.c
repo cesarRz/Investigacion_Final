14e62b5e8bba5bc25d1a8e0523f4c07f6d6a484b
cesarRz <cesarrz2000@gmail.com>
2023-04-10 13:55:41 -0600

    file name change

--- readCsv.c
+++ readCsv.c
@@ -2,10 +2,12 @@
 // C program for the above approach
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdbool.h>
+
 // Driver Code
 int main()
 {
+
     // Variables para las coordenadas x, y, z
     float x = 0;
     float y = 0;
@@ -13,18 +15,25 @@ int main()
     // Leemos el archivo csv
     FILE *fp;
     float rx, ry;
-    fp = fopen ("test.csv", "r");
-    if (fp == NULL) {
-        printf ("El archivo no se puede abrir \n");
-        exit (1);
+
+    while (true)
+    {
+        fp = fopen("coordenadas.csv", "r");
+        if (fp == NULL)
+        {
+            printf("El archivo no se puede abrir \n");
+            exit(1);
+        }
+
+        while (fscanf(fp, "%f, %f", &rx, &ry) != EOF)
+        {
+            x = rx;
+            y = ry;
+        }
+        printf("\nX: %f\n", x);
+        printf("\nY: %f\n", y);
+        fclose(fp);
     }
 
-    while (fscanf (fp, "%f, %f", &rx, &ry) != EOF) {
-        x = rx;
-        y = ry;
-    }
-    fclose(fp);
-    printf("\nX: %f\n", x);
-    printf("\nY: %f\n", y);
     return 0;
 }
